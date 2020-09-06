8b3ebda725475bfa794119096703e86f7967bb89
Mugi4ka <mugi4ka@mail.ru>
2020-09-07 00:15:00 +0300

    update seeding(base data)

--- database/seeds/CurrencySeeder.php
+++ database/seeds/CurrencySeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -19,18 +20,24 @@ class CurrencySeeder extends Seeder
                 'symbol' => '$',
                 'is_main' => 0,
                 'rate' => '0',
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
             ],
             [
                 'code' => 'RUB',
                 'symbol' => '₽',
                 'is_main' => 1,
                 'rate' => '1',
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
             ],
             [
                 'code' => 'EUR',
                 'symbol' => '€',
                 'is_main' => 0,
                 'rate' => '0',
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
             ],
         ]);
     }
